59e0b2bb9bbf1f90c7c70e6d2e9fbd5b73b5f814
Kacper Chrabąszcz <kacper.chrabaszcz@teb.edu.pl>
2024-03-23 16:17:10 +0100

    Add project files.

---
+++ inf04_2022_czerwiec_konsolka/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] tablica = new int[10];
+WczytajTablice();
+for (int i = 0; i < tablica.Length; i++)
+{
+    int maxIndex = Array.IndexOf(tablica, ZnajdzMax(i));
+    tablica[i] = tablica[maxIndex] > tablica[i] ? tablica[maxIndex] : tablica[i];
+}
+Console.WriteLine("Posortowane"); foreach (int liczba in tablica) Console.Write(liczba + " ");
+void WczytajTablice()
+{
+    Console.WriteLine("10 liczb:");
+    for (int i = 0; i < 10; i++) tablica[i] = int.Parse(Console.ReadLine());
+}
+int ZnajdzMax(int start) { return tablica.ToList().GetRange(start, tablica.Length - start).Max(); }
